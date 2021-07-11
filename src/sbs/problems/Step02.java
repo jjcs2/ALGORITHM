58724646a593eb60c4285ecf0fddaad8095d4fc2
werg147 <werg147@gmail.com>
2021-07-11 23:20:53 +0900

    02. if문

--- src/sbs/problems/Step02.java
+++ src/sbs/problems/Step02.java
@@ -63,6 +63,24 @@ public class Step02 {
         } else {
         	System.out.println("0");
         }
+        
+        
+        //14681. 사분면 고르기
+        //Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        //sc.close();
+        
+        if(x>0 && y>0){
+            System.out.println("1");
+        } else if(x<0 && y>0){
+            System.out.println("2");
+        } else if(x<0 && y<0){
+            System.out.println("3");
+        } else {
+            System.out.println("4");
+        }
 		
 
 	}
